04059fb33cb83a29dd816da09074eea9699979c3
Koopa1018 <caleblovestoytrains@gmail.com>
2020-06-26 23:58:20 -0600

    Added OutputToUIText for TMPro-less projects.
    Also, removed OutputToTMPro's [RequireComponent(typeof(TMP_Text))] attribute, since it uses a field instead of GetComponent to find the TMPro component.

--- Text Writer/2. Message Writer/OutputToUIText.cs
+++ Text Writer/2. Message Writer/OutputToUIText.cs
@@ -1,18 +1,18 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-using TMPro;
+using UnityEngine.UI;
 
 namespace Clouds.UI.TextWriting
 {
 	/// <summary>
 	/// Component that outputs text into a TextMesh Pro text field.
 	/// </summary>
-	public class OutputToTMPro : MonoBehaviour, ITextOutput {
+	public class OutputToUIText : MonoBehaviour, ITextOutput {
 		[Tooltip("The text field to output into.")]
-		[SerializeField] TMP_Text textField;
+		[SerializeField] Text textField;
 
 		public void Output (string text) {
 			textField.text = text;
