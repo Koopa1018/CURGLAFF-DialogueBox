32d22b64ce892e2ba562a5545d56bc77cf0a922f
Koopa1018 <caleblovestoytrains@gmail.com>
2022-12-13 23:34:57 -0700

    Add optional sanity checks to queue reader The check triggers if a window tries to open with no messages in the queue. By default, it throws an error in the editor and is bypassed at runtime, but there are compiler flags to set it to silently fail instead, to have it run at runtime, or to have it not run in the editor.

--- Dialogue Box/MessageQueueReader.cs
+++ Dialogue Box/MessageQueueReader.cs
@@ -1,4 +1,11 @@
-﻿using System.Collections;
+﻿#if CLOUDS_MSGBOX_ALWAYS_SANITY_OPENCLOSE || (UNITY_EDITOR && !CLOUDS_MSGBOX_DISABLE_SANITY_OPENCLOSE)
+	#define DO_SANITY_CHECKS
+#endif
+#if CLOUDS_MSGBOX_SILENT_SANITY_OPENCLOSE
+	#define SILENT_SANITY_CHECKS
+#endif
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
@@ -24,9 +31,25 @@ namespace Clouds.UI.DialogueBox {
 		/// <returns>The number of messages remaining in the queue.</returns>
 		public void TryOutputNext () {
 			if (_queue.Count > 0) {
+				//Queue has messages in it. Open window and start
+				//showing them.
 				window.Open();
 				messagePipeStart.Display(_queue.ReadMessage());
 			} else {
+				//Queue has no messages in it. Close window.
+
+#if DO_SANITY_CHECKS
+				//If box is closed already, something has gone wrong.
+				//Throw an error (if enabled).
+				if (!window.isOpen) {
+	#if SILENT_SANITY_CHECKS
+					return;
+	#else
+					throw new System.Exception("Just attempted to open a message with an empty message queue.");
+	#endif
+				}
+#endif
+				//Box is open. Close it.
 				window.Close();
 				windowOpenerChannel.CloseWindow();
 			}
